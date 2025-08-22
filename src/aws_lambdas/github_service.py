d9331a94b301fee26ce0676089fe61e6815b564c
ongzhixian <zhixian@hotmail.com>
2025-08-22 23:39:38 +0800

    Use variables

--- src/aws_lambdas/github_service.py
+++ src/aws_lambdas/github_service.py
@@ -1,35 +1,11 @@
-def bridge_test(event:dict, context):
-    """Refreshes a token (given a valid token)
+def example(event:dict, context):
+    """Example lambda function
     Use case:
-        (authentication)
+        (example) This function is used to demonstrate how to handle an AWS Lambda event.
     """
-    # dump_api_gateway_event_context(event, context)
 
     try:
-        # Validation
-        print('# HELO WORLD')
-
-        # authorization_header = EventHeadersJson.get_authorization_header(event)
-        # if authorization_header is None:
-        #     return endpoint_response.forbidden()
-
-        # token_body = authorization_header.replace('TOKEN', '').strip()
-        # token_is_valid = shared_token_service.verify_token(token_body)
-        # if not token_is_valid:
-        #     return endpoint_response.forbidden()
-
-        # # Parse the token to get the username
-        # username = shared_token_service.get_token_content(token_body)
-
-        # data_object = {
-        #     'token': shared_token_service.generate_token(username )
-        # }
-
-        # return endpoint_response.data(OperationResultMessage(
-        #         operation_is_successful=True,
-        #         message='Token refreshed.',
-        #         data_object=data_object))
-
+        print('Example Lambda function triggered with event:', event)
     except Exception as error:
         print(error)
-        # return endpoint_response.ok(False, f'HAS ERROR: {error}')
+        
